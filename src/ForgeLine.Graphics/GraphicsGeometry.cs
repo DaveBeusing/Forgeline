266050c7b50afffc777ba17d99c5cd8e1cc98fe9
Dave Beusing <david.beusing@gmail.com>
2026-09-24 21:17:39 +0200

    Add graphics geometry contracts

---
+++ src/ForgeLine.Graphics/GraphicsGeometry.cs
@@ -0,0 +1,41 @@
+namespace ForgeLine.Graphics;
+
+public enum GraphicsVertexElementFormat
+{
+    Float2,
+    Float3,
+    Float4
+}
+
+public readonly record struct GraphicsVertexElement(
+    string SemanticName,
+    int SemanticIndex,
+    GraphicsVertexElementFormat Format,
+    int OffsetInBytes)
+{
+    internal void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(SemanticName))
+        {
+            throw new ArgumentException(
+                "A vertex semantic name is required.",
+                nameof(SemanticName));
+        }
+
+        if (SemanticIndex < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(SemanticIndex));
+        }
+
+        if (OffsetInBytes < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(OffsetInBytes));
+        }
+    }
+}
+
+public enum GraphicsIndexFormat
+{
+    UInt16,
+    UInt32
+}
