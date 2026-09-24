71a5d0061bed98561ba67ea11f81c46e2cf7cd44
Dave Beusing <david.beusing@gmail.com>
2026-09-24 11:19:52 +0200

    Integrate jobs with simulation system boundaries

--- src/ForgeLine.Simulation/SimulationContext.cs
+++ src/ForgeLine.Simulation/SimulationContext.cs
@@ -1,18 +1,25 @@
 using ForgeLine.Ecs;
+using ForgeLine.Jobs;
 
 namespace ForgeLine.Simulation;
 
 public sealed class SimulationContext
 {
-    internal SimulationContext(EntityRegistry entities, SimulationRandom random)
+    internal SimulationContext(
+        EntityRegistry entities,
+        SimulationRandom random,
+        JobScheduler? jobScheduler)
     {
         Entities = entities;
         Random = random;
+        Jobs = new SimulationJobs(jobScheduler);
     }
 
     public EntityRegistry Entities { get; }
 
     public SimulationRandom Random { get; }
+
+    public SimulationJobs Jobs { get; }
 
     public SimulationTick Tick { get; internal set; }
 
