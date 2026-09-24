3a05cd02d17ba3ab192311f70fb10edc78c2e43a
Dave Beusing <david.beusing@gmail.com>
2026-09-24 18:14:06 +0200

    Expose pipeline binding and draw commands

--- src/ForgeLine.Graphics/IGraphicsCommandContext.cs
+++ src/ForgeLine.Graphics/IGraphicsCommandContext.cs
@@ -11,4 +11,8 @@ public interface IGraphicsCommandContext
     void SetViewport(float x, float y, float width, float height);
 
     void SetScissor(int left, int top, int right, int bottom);
+
+    void SetPipeline(IGraphicsPipeline pipeline);
+
+    void Draw(int vertexCount, int startVertex = 0);
 }
