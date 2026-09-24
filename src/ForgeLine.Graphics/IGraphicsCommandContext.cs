7fbb0e470225f89f29c25ba243f9cd405536fc47
Dave Beusing <david.beusing@gmail.com>
2026-09-24 21:18:27 +0200

    Add indexed geometry command contracts

--- src/ForgeLine.Graphics/IGraphicsCommandContext.cs
+++ src/ForgeLine.Graphics/IGraphicsCommandContext.cs
@@ -14,5 +14,22 @@ public interface IGraphicsCommandContext
 
     void SetPipeline(IGraphicsPipeline pipeline);
 
+    void SetVertexBuffer(
+        IGraphicsBuffer buffer,
+        int strideInBytes,
+        int offsetInBytes = 0);
+
+    void SetIndexBuffer(
+        IGraphicsBuffer buffer,
+        GraphicsIndexFormat format,
+        int offsetInBytes = 0);
+
+    void SetVertexConstants(ReadOnlySpan<float> values);
+
     void Draw(int vertexCount, int startVertex = 0);
+
+    void DrawIndexed(
+        int indexCount,
+        int startIndex = 0,
+        int baseVertex = 0);
 }
