2c063f063f6b21861b43ff6a6aa6c73b3f1eba6a
Dave Beusing <david.beusing@gmail.com>
2026-09-25 00:18:24 +0200

    Classify local formation movement orders

--- src/ForgeLine.Game/MovementOrder.cs
+++ src/ForgeLine.Game/MovementOrder.cs
@@ -3,8 +3,15 @@ using ForgeLine.Simulation;
 
 namespace ForgeLine.Game;
 
+public enum MovementOrderKind : byte
+{
+    Strategic = 0,
+    FormationLocal = 1
+}
+
 public readonly record struct MovementOrder(
     PlayerId Issuer,
     Vector3 WorldTarget,
     SimulationTick SubmittedAtTick,
-    SimulationTick AcceptedAtTick);
+    SimulationTick AcceptedAtTick,
+    MovementOrderKind Kind = MovementOrderKind.Strategic);
